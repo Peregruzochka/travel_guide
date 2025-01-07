98a9034704a7cb9c96943b80f2766f0f3bc65331
Peregruzochka <peregruzochka48@mail.ru>
2025-01-07 15:50:35 +0300

    отключил spring boot test

--- src/test/java/ru/peregruzochka/travel_guide/TravelGuideApplicationTests.java
+++ src/test/java/ru/peregruzochka/travel_guide/TravelGuideApplicationTests.java
@@ -3,7 +3,7 @@ package ru.peregruzochka.travel_guide;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TravelGuideApplicationTests {
 
 	@Test
